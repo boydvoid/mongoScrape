59aff9457eb9e5c2f7ccd1e5a5918879dabb0587
Bobby Boyd <bboyd2008@gmail.com>
2019-01-12 10:57:30 -0800

    server running

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-$(document).ready(function () {
+var express = require('express');
+var exphbs = require('express-handlebars');
 
+var app = express();
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-})
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(3000, function () {
+  console.log("App running on port 3000!");
+});
